f4729d0d5d272e286aa9ad269795edd3eadb4c4c
Djangirkhan Tuleov <89830596+jeanguir@users.noreply.github.com>
2024-08-08 20:33:34 +0500

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,1 +1,45 @@
+package com.example.webfluxdemo;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Event {
+
+    @Id
+    private String id;
+    private String name;
+    private String location;
+
+    public Event() {
+    }
+
+    public Event(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
