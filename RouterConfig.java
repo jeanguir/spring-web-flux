99f136000dd7015b6da9026360d2faf41a085de5
Djangirkhan Tuleov <89830596+jeanguir@users.noreply.github.com>
2024-08-10 23:44:16 +0500

    Update RouterConfig.java

--- RouterConfig.java
+++ RouterConfig.java
@@ -1,1 +1,27 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(EventHandler handler) {
+        return RouterFunctions
+                .nest(path("/events"),
+                        route(GET("/"), handler::getAllEvents)
+                                .andRoute(GET("/{id}"), handler::getEventById)
+                                .andRoute(POST("/"), handler::createEvent)
+                                .andRoute(PUT("/{id}"), handler::updateEvent)
+                                .andRoute(DELETE("/{id}"), handler::deleteEvent)
+                );
+    }
+}
 
