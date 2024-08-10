5b15d81c4808520d9c40601c7955f2dcf30515bd
Djangirkhan Tuleov <89830596+jeanguir@users.noreply.github.com>
2024-08-10 23:43:40 +0500

    Update EventRepository.java

--- EventRepository.java
+++ EventRepository.java
@@ -1,1 +1,9 @@
+package org.example;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+public interface EventRepository extends ReactiveMongoRepository<Event, String> {
+    Mono<Event> findByName(String name);
+}
 
